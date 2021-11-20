fb3c840ae0ba0a2d4e29696a8896e8966d637292
Alexandra-Rybakova <alexandra.rybakova@coxautoinc.com>
2021-11-20 21:21:11 +0300

    Delete sending an email

--- AutomationPracticeProject/TestCases/BaseSuite.cs
+++ AutomationPracticeProject/TestCases/BaseSuite.cs
@@ -21,14 +21,6 @@ namespace AutomationPracticeProject.TestCases
             try
             {
                 Extent.Close();
-
-                var senderNickname = "Lizy Flower";
-                var from = "lizy.flower22@gmail.com";
-                var to = "lizy.flower22@gmail.com";
-                var subject = "Test results!";
-                var body = "Test run is over! The report is attached to the letter.";
-                var attachmentPath = "ExtentReports.html";
-                GMailHelper.SendMessageWithAttachment(senderNickname, from, to, subject, body, attachmentPath);
             }
             catch (Exception exc)
             {
